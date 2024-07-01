c81cef3d380262158440919d1f98b09f3749d2ad
AnujNayak108 <86143398+AnujNayak108@users.noreply.github.com>
2024-07-01 15:25:08 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
     </div>
     </>
     return (
-        <nav classname="fixed">
+        <nav classname="fixed top-0 left-0">
             <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4 border-b border-slate-800">
                 <div className="flex items-center flex-1">
                     <span className="text-3xl font-bold">
