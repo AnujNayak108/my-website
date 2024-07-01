d29e78e914cb82efcfeff7bf66dbe98e64e1ae07
AnujNayak108 <86143398+AnujNayak108@users.noreply.github.com>
2024-07-01 15:31:22 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,8 +32,8 @@ const NavBar = () => {
     </div>
     </>
     return (
-        <nav classname="fixed w-full top-0 left-0">
-            <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4 border-b border-slate-800">
+        <nav>
+            <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4 border-b border-slate-800 fixed w-full top-0 left-0">
                 <div className="flex items-center flex-1">
                     <span className="text-3xl font-bold">
                         <FaAutoprefixer className="h-11 w-11"/>
